14bbaba34a78d283bf9a26e4afa37bf7ecb4160f
Gourangsharma <95042000+Gourangsharma@users.noreply.github.com>
2025-04-24 15:29:59 +0530

    Add files via upload

---
+++ Database setup and analysis.sql
@@ -0,0 +1,23 @@
+CREATE TABLE netflix
+(
+    show_id      VARCHAR(5),
+    type         VARCHAR(10),
+    title        VARCHAR(250),
+    director     VARCHAR(550),
+    casts        VARCHAR(1050),
+    country      VARCHAR(550),
+    date_added   VARCHAR(55),
+    release_year INT,
+    rating       VARCHAR(15),
+    duration     VARCHAR(15),
+    listed_in    VARCHAR(50),
+    description  VARCHAR(550)
+);
+
+select * from netflix;
+
+select count(*) from netflix;
+
+select distinct type from netflix;
+
+select count(show_id) from netflix;
